180c32f9f16ca26d4e5df985bf9fd08bf6069478
portes <fagundes.daniel191@gmail.com>
2024-08-18 17:25:07 -0300

    realiza a Tarefa 3: Contagem de Ocorrências de cada palavra

--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -4,3 +4,11 @@ export function transformData(users) {
         .map(user => ({ id: user.id, name: user.name }))
         .sort((a, b) => a.name.localeCompare(b.name));
 }
+
+// Tarefa 3: Contagem de Ocorrências de cada palavra
+export function countOccurrences(words) {
+    return words.reduce((acc, word) => {
+        acc[word] = acc[word] ? acc[word] + 1 : 1;
+        return acc;
+    }, {});
+}
--- tests/unit/arrayUtils.spec.js
+++ tests/unit/arrayUtils.spec.js
@@ -1,20 +1,35 @@
-import {transformData} from '@/utils/arrayUtils';
+import {countOccurrences, transformData} from '@/utils/arrayUtils';
 
+// Tarefa 1: Testar a função transformData
 describe('arrayUtils', () => {
     describe('transformData', () => {
         it('deve filtrar usuários ativos, manter apenas id e nome e ordernar pelo nome', () => {
             const users = [
-                { id: 1, name: 'Alice', age: 25, isActive: true },
-                { id: 2, name: 'Bob', age: 30, isActive: false },
-                { id: 3, name: 'Charlie', age: 22, isActive: true }
+                {id: 1, name: 'Alice', age: 25, isActive: true},
+                {id: 2, name: 'Bob', age: 30, isActive: false},
+                {id: 3, name: 'Charlie', age: 22, isActive: true}
             ];
             const result = transformData(users);
             expect(result).toEqual([
-                { id: 1, name: 'Alice' },
-                { id: 3, name: 'Charlie' }
+                {id: 1, name: 'Alice'},
+                {id: 3, name: 'Charlie'}
             ]);
         });
     });
 });
 
+// Tarefa 3: Testar Contagem de Ocorrências de cada palavra
+describe('arrayUtils', () => {
+    describe('countOccurrences', () => {
+        it('deve contar a ocorrência de cada palavra', () => {
+            const words = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
+            const result = countOccurrences(words);
+            expect(result).toEqual({
+                apple: 3,
+                banana: 2,
+                orange: 1
+            });
+        });
+    });
+});
 
